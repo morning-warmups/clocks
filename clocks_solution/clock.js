3c2e34d5c56d0d9fdc3795f7b90ff54d6a858d43
Matt Swann <matt@bfa.industries>
2016-01-05 10:08:12 +1100

    added solution

---
+++ clocks_solution/clock.js
@@ -0,0 +1,41 @@
+$(function(){
+
+function clock(){
+  var currentTime = new Date();
+
+  // Every hour the hour hand moves 30 degrees,
+  // and 0.5 additional degrees for each past minute
+  var currentHour = currentTime.getHours() * 30 + currentTime.getMinutes() * (0.5);
+  var currentMin = currentTime.getMinutes() * 6;
+  var currentSec = currentTime.getSeconds() * 6;
+
+  var minute = $("#minute-hand").hide()
+                                .css("transform","rotate("+currentMin+"deg)")
+                                .fadeIn(1500);
+  var second = $("#second-hand").hide()
+                                .css("transform","rotate("+currentSec+"deg)")
+                                .fadeIn(1500);
+  var hour = $("#hour-hand").hide()
+                            .css("transform","rotate("+currentHour+"deg)")
+                            .fadeIn(1500);
+
+  start();
+
+  function start(){
+    setInterval(function(){
+      currentSec += 6;
+      second.css("transform","rotate("+ currentSec +"deg)");
+    },1000);
+    setInterval(function(){
+      currentMin += .1;
+      minute.css("transform","rotate("+ currentMin +"deg)");
+    },1000);
+    setInterval(function(){
+      currentHour += .008;
+      hour.css("transform","rotate("+ currentHour +"deg)");
+    },1000);
+  }
+}
+
+clock();
+});
